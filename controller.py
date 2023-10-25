67bec61c1048072ecaca3519417c14e3024f3928
rndm <rndm@localhost.localdomain>
2023-10-25 12:09:56 +0000

    Added viewing deletion editing of endpoints

--- controller.py
+++ controller.py
@@ -1,12 +1,77 @@
 # import wfuzz
 import validators
+import json
+
+from enum import StrEnum
+from imgui_bundle import imgui_color_text_edit as ed
+TextEditor = ed.TextEditor
 
 
-class EndPoint:
-    def __init__(self, url: str = "https://example.com/some-action", get: bool = False, post: bool = False):
+class HttpType(StrEnum):
+    GET = "GET"
+    POST = "POST"
+
+
+class EdType(StrEnum):
+    JSON = "JSON"
+    HTML = "HTML"
+
+
+class EdText:
+    def __init__(self, type: EdType, text: str):
+        self.type = type
+        self.text = text
+
+    def render_ed(self, editor: TextEditor, label: str):
+        editor.set_text(self.text)
+        if self.type == EdType.JSON:
+            editor.set_language_definition(TextEditor.LanguageDefinition.json())
+        if self.type == EdType.HTML:
+            editor.set_language_definition(None)
+        editor.render(label)
+        self.text = editor.get_text()
+
+    def validate(self) -> str:
+        if self.type == EdType.JSON:
+            try:
+                json.loads(self.text)
+            except Exception as ex:
+                return str(ex)
+        return ""
+        
+
+JSONExample = EdText(EdType.JSON, """
+{
+    "json": "example",
+    "isjson": true
+}""")
+
+
+class Interaction:
+    def __init__(self, type: HttpType, request: EdText = JSONExample, response: EdText = JSONExample):
+        self.type = type
+        self.request = request
+        self.response = response
+
+    def validate(self):
+        ret = self.request.validate()
+        if ret != "":
+            return ret
+        ret = self.response.validate()
+        if ret != "":
+            return ret
+        return ""
+
+
+class Endpoint:
+    def __init__(self, url: str = "https://example.com/some-action",
+                 get: bool = False, get_interaction: Interaction = Interaction(HttpType.GET),
+                 post: bool = False, post_interaction: Interaction = Interaction(HttpType.POST)):
         self.url = url
         self.get = get
+        self.get_interaction = get_interaction
         self.post = post
+        self.post_interaction = post_interaction
 
     def http_types(self) -> str:
         ret = ""
@@ -23,12 +88,24 @@ class EndPoint:
     def validate(self) -> str:
         if not validators.url(self.url):
             return "endpoint url must be a ... url?!?!"
+        
+        if not (self.get or self.post):
+            return "endpoint must have at least a single http request"
+        
+        ret = self.get_interaction.validate()
+        if ret != "":
+            return ret
+        
+        ret = self.post_interaction.validate()
+        if ret != "":
+            return ret
+
         return ""
 
 
 class Controller:
-    def __init__(self):
-        self.end_points = []
+    def __init__(self, endpoints: list[Endpoint] = []):
+        self.endpoints = endpoints
 
-    def add_endpoint(self, endpoint: EndPoint):
-        self.end_points.append(endpoint)
+    def add_endpoint(self, endpoint: Endpoint):
+        self.endpoints.append(endpoint)
--- view.py
+++ view.py
@@ -1,14 +1,53 @@
 from functools import partial
-from imgui_bundle import imgui, immapp
-from controller import (Controller, EndPoint)
+from imgui_bundle import imgui, immapp, imgui_color_text_edit as ed
+from controller import (Controller, Endpoint)
+TextEditor = ed.TextEditor
 
 
-class TestInput:
+class EndpointInput:
+    editor = TextEditor()
+    validation = ""
+        
+    @staticmethod
+    def edit(endpoint: Endpoint, label: str) -> bool:
+        if endpoint is None:
+            return False
+
+        ret = False
+
+        if not imgui.is_popup_open(label):
+            imgui.open_popup(label)
+        if imgui.begin_popup_modal(label):
+            changed, endpoint.url = imgui.input_text("URL", endpoint.url)
+            
+            changed, endpoint.get = imgui.checkbox("GET", endpoint.get)
+            if endpoint.get and imgui.tree_node("GET request"):
+                endpoint.get_interaction.request.render_ed(EndpointInput.editor, "GET request")
+                imgui.tree_pop()
+            
+            changed, endpoint.post = imgui.checkbox("POST", endpoint.post)
+            if endpoint.post and imgui.tree_node("POST request"):
+                endpoint.post_interaction.request.render_ed(EndpointInput.editor, "POST request")
+                imgui.tree_pop()
+
+            if imgui.button("Save"):
+                EndpointInput.validation = endpoint.validate()
+                if EndpointInput.validation == "":
+                    ret = True
+
+            if EndpointInput.validation != "":
+                imgui.text_colored(imgui.ImVec4(255, 0, 0, 255), f"Failed to validate input:\n{EndpointInput.validation}")
+            imgui.end_popup()
+
+        return ret
+
+
+class TestInputWindow:
     def __init__(self, parent):
         self.controller = parent.controller
-        self.endpoint = EndPoint("https://example.com/some-action/")
+        self.endpoint_add = None
+        self.endpoint_edit = None
         self.validation = ""
-        self.http_type = 0
 
     def gui(self):
         imgui.begin("Test input selection")
@@ -20,32 +59,49 @@ You can set which http requests to make, input payload templates and output exam
 Here you can also choose for which security vulnerabilities to test for and what should be servers response.
                        """)
             imgui.tree_pop()
-        
-        changed, self.endpoint.url = imgui.input_text("URL", self.endpoint.url)
-        
-        imgui.text(f"{self.endpoint.http_types()}: ")
-        imgui.same_line()
-        if imgui.tree_node("Http types"):
-            changed, self.endpoint.get = imgui.checkbox("GET", self.endpoint.get)
-            changed, self.endpoint.post = imgui.checkbox("POST", self.endpoint.post)
-            imgui.tree_pop()
 
         if imgui.button("Add endpoint"):
-            self.validation = self.endpoint.validate()
-            if self.validation == "":
-                self.controller.add_endpoint(self.endpoint)
+            self.endpoint_add = Endpoint()
+            
+        if EndpointInput.edit(self.endpoint_add, "Add endpoint"):
+            self.controller.add_endpoint(self.endpoint_add)
+            self.endpoint_add = None
+            print("Added endpoint")
 
-        if self.validation != "":
-            imgui.text_colored(imgui.ImVec4(255, 0, 0, 255), f"Failed to validate input:\n{self.validation}")
+        if imgui.begin_table("Endpoints", 3, View.table_flags):
+            imgui.table_header("URL")
+            imgui.table_header("HTTP")
+            imgui.table_header("Actions")
+
+            for ep in self.controller.endpoints:
+                imgui.table_next_column()
+                imgui.text(ep.url)
+
+                imgui.table_next_column()
+                imgui.text(ep.http_types())
+                
+                imgui.table_next_column()
+                if imgui.button("Edit"):
+                    self.endpoint_edit = ep
+                imgui.same_line()
+                if imgui.button("Delete"):
+                    self.controller.endpoints.remove(ep)
+            imgui.end_table()
+
+        if EndpointInput.edit(self.endpoint_edit, "Edit endpoint"):
+            self.endpoint_edit = None
+            print("Added endpoint")
 
         imgui.end()
 
 
 class View:
+    table_flags = imgui.TableFlags_.scroll_y | imgui.TableFlags_.row_bg | imgui.TableFlags_.borders_outer | imgui.TableFlags_.borders_v | imgui.TableFlags_.resizable | imgui.TableFlags_.reorderable | imgui.TableFlags_.hideable
+
     def __init__(self):
         self.controller = Controller()
         self.windows = [
-            TestInput(self)
+            TestInputWindow(self)
         ]
 
     def run(self):
